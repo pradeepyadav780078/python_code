2899d7c20807d1b801e19f6c87ca13399cd77e81
Pradeep Yadav <pradeepyadav4614838@gmail.com>
2025-09-18 23:49:50 +0530

    Update leapyearornot.py

--- leapyearornot.py
+++ leapyearornot.py
@@ -1,5 +1,6 @@
 year = int(input("Enter a year:"))
-if ("year % 4 == 0") or ("year % 400 == 0"and " year% 100 != 0"):
+if (year % 4 == 0) or (year % 400 == 0 and  year% 100 != 0):
     print("leap year")
 else:
     print("not leap year")
+
